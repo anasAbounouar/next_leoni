4d5ce52d5b177f9293a5efdbdd2a349eefe42d5f
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-08-31 00:28:19 +0100

    little changes and update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
     eslint: {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
   };
-  
-  export default nextConfig;
   
