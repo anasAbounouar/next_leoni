9ca34dfae66d9a831eb01497941752fa4c0bea80
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-09-01 12:29:45 +0100

    clerk installed successfully && slight changes in the workflow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-// src/components/Navbar.jsx
 'use client';
 import React from 'react';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
-import { AcmeLogo } from '/public/images/Logo.jsx';
+import { AcmeLogo } from '/public/images/Logo.jsx';  // Ensure this path is correct
 import { usePathname } from 'next/navigation';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export default function NavbarComponent({ ...props }) {
   const pathname = usePathname();
@@ -12,20 +12,19 @@ export default function NavbarComponent({ ...props }) {
     <Navbar shouldHideOnScroll {...props}>
       <NavbarBrand>
         <AcmeLogo />
-        {/* <p className="font-bold text-inherit">LEONI</p> */}
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem isActive={pathname === '/'}>
-          <Link 
+          <Link
             color={pathname === '/' ? 'primary' : 'foreground'}
-            href="/" 
+            href="/"
             aria-current={pathname === '/' ? 'page' : undefined}
           >
             Home
           </Link>
         </NavbarItem>
         <NavbarItem isActive={pathname === '/services'}>
-          <Link 
+          <Link
             color={pathname === '/services' ? 'primary' : 'foreground'}
             href="/services"
             aria-current={pathname === '/services' ? 'page' : undefined}
@@ -34,7 +33,7 @@ export default function NavbarComponent({ ...props }) {
           </Link>
         </NavbarItem>
         <NavbarItem isActive={pathname === '/about'}>
-          <Link 
+          <Link
             color={pathname === '/about' ? 'primary' : 'foreground'}
             href="/about"
             aria-current={pathname === '/about' ? 'page' : undefined}
@@ -43,7 +42,7 @@ export default function NavbarComponent({ ...props }) {
           </Link>
         </NavbarItem>
         <NavbarItem isActive={pathname === '/contact'}>
-          <Link 
+          <Link
             color={pathname === '/contact' ? 'primary' : 'foreground'}
             href="/contact"
             aria-current={pathname === '/contact' ? 'page' : undefined}
@@ -53,13 +52,15 @@ export default function NavbarComponent({ ...props }) {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+        <NavbarItem>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
