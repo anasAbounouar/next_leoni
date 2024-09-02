fc6a9f029200111a6fb12dfe95b4b7395331b2ba
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-09-03 00:14:57 +0100

    trying to fix the build error 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
-import TerserPlugin from 'terser-webpack-plugin';
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-          },
-        }),
-      ];
-    }
-    return config;
   },
 };
 
