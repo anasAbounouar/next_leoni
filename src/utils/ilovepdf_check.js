b774f00e8ac35f35a178a168e5f9ef02d871e0e8
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-09-05 00:34:04 +0100

    feat(Check ILovePDF API : handle failure changes)

--- src/utils/ilovepdf_check.js
+++ src/utils/ilovepdf_check.js
@@ -1,8 +1,10 @@
-
+import fs from 'fs';
 import ILovePDFApi from '@ilovepdf/ilovepdf-nodejs';
-import minimist from 'minimist'; // Use import for minimist
+import ILovePDFFile from '@ilovepdf/ilovepdf-nodejs/ILovePDFFile';
+import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
+
 const publicKey = args['publicKey'];
 const secretKey = args['secretKey'];
 
@@ -15,27 +17,28 @@ const ilovePDFInstance = new ILovePDFApi(publicKey, secretKey);
 
 async function checkAPIStatusWithConversion(imageFilePath) {
   try {
-    const task = ilovePDFInstance.newTask('imagepdf');  
-    await task.start(); // Start the task
+    const task = ilovePDFInstance.newTask('imagepdf');
+    await task.start();
 
-    await task.addFile(imageFilePath); // Add an image file (to check API points)
-    await task.process(); // Try to process the file
+    const file = new ILovePDFFile(imageFilePath);
+    await task.addFile(file);
 
-    const pdfBuffer = await task.download(); // Attempt to download the resulting PDF
+    await task.process();
+    const pdfBuffer = await task.download();
 
     console.log('API is working. Conversion successful.');
-    process.stdout.write('::set-output name=apiStatus::true\n');
-    process.exit(0); // Exit with code 0 to indicate success
+    fs.appendFileSync(process.env.GITHUB_ENV, `apiStatus=true\n`);
+    process.exit(0);
   } catch (error) {
     console.error('Error during API conversion or no points left:', error.message);
-    process.stdout.write('::set-output name=apiStatus::false\n');
-    process.exit(1); // Exit with code 1 to indicate failure
+    fs.appendFileSync(process.env.GITHUB_ENV, `apiStatus=false\n`);
+    process.exit(1);
   }
 }
 
 (async () => {
   if (args['check-api']) {
-    const imagePath = 'public/next.svg'; // Default image path for testing conversion
+    const imagePath = 'public/next.svg';
     await checkAPIStatusWithConversion(imagePath);
   } else {
     console.error('No valid arguments provided.');
