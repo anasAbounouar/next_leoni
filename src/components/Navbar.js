0b3e0ba369e592da5e35fd9bfd60b446d089db2a
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-09-10 13:07:36 +0100

    moified the isActive in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from '@nextui-org/react';
-import { AcmeLogo } from '/public/images/Logo.jsx';  // Ensure this path is correct
+import { AcmeLogo } from '/public/images/Logo.jsx';  // Ensure this path is correct
 import { usePathname } from 'next/navigation';
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
@@ -14,38 +14,38 @@ export default function NavbarComponent({ ...props }) {
         <AcmeLogo />
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem isActive={pathname === '/'}>
+        <NavbarItem isActive={pathname==='/'}>
           <Link
-            className={pathname === '/' ? 'text-leoniAccent' : 'text-white'}
+            className={pathname==='/' ? 'text-leoniAccent' : 'text-white'}
             href="/"
-            aria-current={pathname === '/' ? 'page' : undefined}
+            aria-current={pathname==='/' ? 'page' : undefined}
           >
             Home
           </Link>
         </NavbarItem>
-        <NavbarItem isActive={pathname === '/services'}>
+        <NavbarItem isActive={pathname.startsWith('/services')}>
           <Link
-            className={pathname === '/services' ? 'text-leoniAccent' : 'text-white'}
+            className={pathname.startsWith('/services') ? 'text-leoniAccent' : 'text-white'}
             href="/services"
-            aria-current={pathname === '/services' ? 'page' : undefined}
+            aria-current={pathname.startsWith('/services') ? 'page' : undefined}
           >
             Services
           </Link>
         </NavbarItem>
-        <NavbarItem isActive={pathname === '/about'}>
+        <NavbarItem isActive={pathname.startsWith('/about')}>
           <Link
-            className={pathname === '/about' ? 'text-leoniAccent' : 'text-white'}
+            className={pathname.startsWith('/about') ? 'text-leoniAccent' : 'text-white'}
             href="/about"
-            aria-current={pathname === '/about' ? 'page' : undefined}
+            aria-current={pathname.startsWith('/about') ? 'page' : undefined}
           >
             About
           </Link>
         </NavbarItem>
-        <NavbarItem isActive={pathname === '/contact'}>
+        <NavbarItem isActive={pathname.startsWith('/contact')}>
           <Link
-            className={pathname === '/contact' ? 'text-leoniAccent' : 'text-white'}
+            className={pathname.startsWith('/contact') ? 'text-leoniAccent' : 'text-white'}
             href="/contact"
-            aria-current={pathname === '/contact' ? 'page' : undefined}
+            aria-current={pathname.startsWith('/contact') ? 'page' : undefined}
           >
             Contact
           </Link>
@@ -59,11 +59,11 @@ export default function NavbarComponent({ ...props }) {
         </NavbarItem>
         <NavbarItem>
           <SignedOut>
-          <SignInButton>
-        <button className="px-3 py-1 bg-blue-500 text-white font-semibold text-lg rounded-lg shadow-md hover:bg-blue-600 transition-colors">
-          Sign in 
-      </button>
-    </SignInButton>
+            <SignInButton>
+              <button className="px-3 py-1 bg-blue-500 text-white font-semibold text-lg rounded-lg shadow-md hover:bg-blue-600 transition-colors">
+                Sign in
+              </button>
+            </SignInButton>
           </SignedOut>
         </NavbarItem>
       </NavbarContent>
