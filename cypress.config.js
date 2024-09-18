b10cc310eeef6fb8d25dd657b48d23b51ea26660
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-09-18 19:30:12 +0100

    feat(cypress git actions): created a results folder for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
     },
     baseUrl: 'http://localhost:3000',
     defaultCommandTimeout: 10000, // Increase timeout to 10 seconds
+    reporter: 'junit', // Use a reporter that outputs to a file
+    reporterOptions: {
+      mochaFile: 'cypress/results/results-[hash].xml', // Output results in this path
+      toConsole: true // Optional: also output to the console
+    }
   },
 });
